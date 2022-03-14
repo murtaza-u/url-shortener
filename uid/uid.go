b6106022146723e068083d09265b888b45d1b7e7
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-14 21:27:41 +0530

    Get new UID

--- uid/uid.go
+++ uid/uid.go
@@ -1,29 +1,23 @@
 package uid
 
-import (
-	"crypto/rand"
-	"io"
-)
+import "github.com/murtaza-udaipurwala/sme/db"
 
-var chars = []byte{
-	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
-	'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-	'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-	'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-	'y', 'z',
-}
+func NewUID(db *db.DB) (string, error) {
+	var uid string
+	var err error
 
-func NewUID() (string, error) {
-	b := make([]byte, 6)
-	_, err := io.ReadAtLeast(rand.Reader, b, 6)
+	for {
+		uid, err = genUID()
+		if err != nil {
+			return "", err
+		}
 
-	if err != nil {
-		return "", err
+		if uidExists(db, uid) {
+			continue
+		}
+
+		break
 	}
 
-	for i := 0; i < len(b); i++ {
-		b[i] = chars[int(b[i])%len(chars)]
-	}
-
-	return string(b), nil
+	return uid, nil
 }
