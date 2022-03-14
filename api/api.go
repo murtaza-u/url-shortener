d1cb3901aa039c16c71969bf2d9054918930136d
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-14 22:13:49 +0530

    modify endpoint

--- api/api.go
+++ api/api.go
@@ -1,20 +1,24 @@
 package api
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/murtaza-udaipurwala/sme/db"
 )
 
 func setRoutes(app *fiber.App) {
 	app.Post("/shorten", handlePOST)
-	app.Get("/shorten/:id", handleGET)
+	app.Get("/:uid", handleGET)
 }
 
 var database *db.DB
 
-func main() {
+func Run() {
 	database = db.InitDB()
 
 	app := fiber.New()
 	setRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
 }
