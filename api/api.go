10dda3ebdeb83721f2a080538e77d92d68d9b78b
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-03-14 21:50:10 +0530

    instantiate DB

--- api/api.go
+++ api/api.go
@@ -1,13 +1,20 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/murtaza-udaipurwala/sme/db"
+)
 
 func setRoutes(app *fiber.App) {
 	app.Post("/shorten", handlePOST)
 	app.Get("/shorten/:id", handleGET)
 }
 
+var database *db.DB
+
 func main() {
+	database = db.InitDB()
+
 	app := fiber.New()
 	setRoutes(app)
 }
